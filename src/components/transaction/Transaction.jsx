e0272d58c2f898a62751bc850922f3fd6157d785
Abadullah Khan <abadullahk73@gmail.com>
2023-12-31 13:04:44 +0530

    Added the remove transaction button

--- src/components/transaction/Transaction.jsx
+++ src/components/transaction/Transaction.jsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import "./transaction.scss";
+import { GlobalContext } from "../../context/GlobalState";
 
 const Transaction = ({ data, index }) => {
+  const { dispatch } = useContext(GlobalContext);
+  const [toggleButton, setToggleButton] = useState(false);
   return (
-    <div className="card">
-      <span className="index">
-        {index + 1}.<span className="title">{data.title}</span>
-      </span>
-      <span className="amount">
-        {data.transaction_type === "expense" ? "-" : "+"}${data.amount}
-      </span>
-    </div>
+    <>
+      <div className={`card `} onClick={() => setToggleButton(!toggleButton)}>
+        <span className="index">
+          {index + 1}.<span className="title">{data.title}</span>
+        </span>
+        <span className="amount">
+          {data.transaction_type === "expense" ? "-" : "+"}${data.amount}
+        </span>
+        <span></span>
+      </div>
+      <div
+        className={`
+        button_container
+   ${toggleButton ? " reveal_button" : ""}
+        `}
+      >
+        <button
+          className="remove_button"
+          title="Remove item"
+          onClick={() =>
+            dispatch({ type: "REMOVE_TRANSACTION", payload: data.id })
+          }
+        >
+          Remove
+        </button>
+      </div>
+    </>
   );
 };
 
